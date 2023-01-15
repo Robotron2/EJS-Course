3ce745f09b8c8edab6ff7d22637ccda95dad470a
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-15 08:50:43 +0100

    For loop in EJS implemented to render todos

--- app.js
+++ app.js
@@ -6,36 +6,24 @@ app.set("view engine", "ejs") //important!!
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-	let today = new Date().getDay()
-	let day = ""
-	switch (today) {
-		case 0:
-			day = "Sunday"
-			break
-		case 1:
-			day = "Monday"
-			break
-		case 2:
-			day = "Tuesday"
-			break
-		case 3:
-			day = "Wednesday"
-			break
-		case 4:
-			day = "Thursday"
-			break
-		case 5:
-			day = "Friday"
-			break
-		case 6:
-			day = "Saturday"
-			break
+let items = []
 
-		default:
-			break
+app.get("/", (req, res) => {
+	let today = new Date()
+	let options = {
+		weekday: "long",
+		// year: "numeric",
+		month: "long",
+		day: "numeric"
 	}
-	res.render("list", { kindOfDay: day })
+	let day = today.toLocaleDateString("en-US", options)
+	res.render("list", { kindOfDay: day, newListItems: items })
+})
+
+app.post("/", (req, res) => {
+	let item = req.body.newItem
+	items.push(item)
+	res.redirect("/")
 })
 
 app.listen(4000, () => {
